7106ff797d8d6156a98d61ae398fd3d1c05ad3e3
Elias700 <elias_ribeiro07@outlook.com.br>
2025-10-06 16:34:09 -0300

    style: styling of the login/signup buttons and visual adjustments on the homepage

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -17,9 +17,42 @@ const Header = () => {
                         <li>Contato</li>
                     </ul>
                 </nav>
-                <div className='[&>button]:text-[var(--color-primary-900)] flex gap-5'>
-                    <button onClick={() => navigate("/login")} className='bg-[var(--color-primary-50)] py-3 px-5 rounded-4xl text-2xl cursor-pointer font-semibold shadow-md transition-colors hover:bg-[var(--color-primary-800)] hover:text-[var(--color-primary-50)] duration-[500ms]'>Login</button>
-                    <button onClick={() => navigate("/cadastro")} className='bg-[var(--color-primary-50)] py-3 px-5 rounded-4xl text-2xl cursor-pointer font-semibold shadow-md transition-colors  hover:bg-[var(--color-primary-800)] hover:text-[var(--color-primary-50)] duration-[500ms] '>Cadastro</button>
+                <div className=' flex gap-5'>
+                    <button onClick={() => navigate("/login")}
+                        className="cursor-pointer   px-12 py-3 rounded-xl border-[1px] border-white text-white font-medium group shadow-md transition-colors hover:bg-[var(--color-primary-800)]"
+                        >
+                        <div className="relative overflow-hidden">
+                            <p
+                            className="group-hover:-translate-y-7 duration-[1.125s] ease-[cubic-bezier(0.19,1,0.22,1)]"
+                            >
+                            Login
+                            </p>
+                            <p
+                            className="absolute top-7 left-0 group-hover:top-0 duration-[1.125s] ease-[cubic-bezier(0.19,1,0.22,1)]"
+                            >
+                            Login
+                            </p>
+                        </div>
+                    </button>
+
+                    <button onClick={() => navigate("/cadastro")}
+                        className="cursor-pointer   px-12 py-3 rounded-xl border-[1px] border-white text-white font-medium group shadow-md transition-colors hover:bg-[var(--color-primary-800)]"
+                        >
+                        <div className="relative overflow-hidden">
+                            <p
+                            className="group-hover:-translate-y-7 duration-[1.125s] ease-[cubic-bezier(0.19,1,0.22,1)]"
+                            >
+                            Cadastro
+                            </p>
+                            <p
+                            className="absolute top-7 left-0 group-hover:top-0 duration-[1.125s] ease-[cubic-bezier(0.19,1,0.22,1)]"
+                            >
+                            Cadastro
+                            </p>
+                        </div>
+                    </button>
+                    
+                    {/* <button onClick={() => navigate("/cadastro")} className='bg-[var(--color-primary-50)] py-3 px-5 rounded-4xl text-2xl cursor-pointer font-semibold shadow-md transition-colors  hover:bg-[var(--color-primary-800)] hover:text-[var(--color-primary-50)] duration-[500ms] '>Cadastro</button> */}
                 </div>
             </div>
         </header>
--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,8 +1,8 @@
 import BeachVideo from "../../assets/video-beach.mp4";
 
-function Header() {
+function Home() {
   return (
-    <header>
+    <div>
       <div className="relative w-full h-[85vh] overflow-hidden">
         <video
           src={BeachVideo}
@@ -18,19 +18,21 @@ function Header() {
         <div className="relative z-10 flex flex-col items-center justify-center h-full text-center text-white px-4 bg-black/40">
           <h1 className="text-4xl font-bold mb-4">Bem-vindo ao Rota do Mar</h1>
           <p className="max-w-2xl mb-6">
-            O PraiaFácil é o seu guia digital para descobrir e aproveitar as
+            O Rota do Mar é o seu guia digital para descobrir e aproveitar as
             melhores praias da sua cidade. Aqui, você encontra informações
             completas sobre cada praia.
           </p>
           <input
-            type="text"
+            className="bg-zinc-200 text-zinc-600 font-mono ring-1 w-100 ring-zinc-400 focus:ring-2 focus:ring-[var(--color-primary-600)] outline-none duration-300 placeholder:text-zinc-600 placeholder:opacity-50 rounded-full px-4 py-1 shadow-md focus:shadow-lg focus:shadow-blue-300"
             placeholder="Pesquise sua praia preferida"
-            className="px-4 py-2 rounded-lg w-100 border-1"
+            name="text"
+            type="text"
           />
+
         </div>
       </div>
-    </header>
+    </div>
   );
 }
 
-export default Header;
+export default Home;
