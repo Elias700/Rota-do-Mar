660b15f4b6428de5e3a7ba80a4cfe81a29fb6902
Elias700 <elias_ribeiro07@outlook.com.br>
2025-10-22 20:18:44 -0300

    feat: Fixes the header at the top of the page, hover modification.

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -2,33 +2,43 @@ import Logo from '../../assets/logo.png'
 import { useNavigate } from 'react-router-dom';
 
 const Header = () => {
-
     const navigate = useNavigate();
 
     return (
-        <header>
-            <div className="bg-[var(--color-primary-600)] flex justify-around items-center p-5 shadow-2xl">
-                <img src={Logo} alt="Logo Rota do Mar" className='h-25'/>
+        // Adicione as classes 'fixed', 'top-0', 'w-full' e 'z-50' aqui
+        <header className="fixed top-0 w-full z-50">
+            <div className="bg-[var(--color-primary-600)] flex justify-around items-center p-2 shadow-2xl">
+                <img src={Logo} alt="Logo Rota do Mar" className='h-20'/>
                 <nav className='flex'>
-                    <ul className="flex gap-10 [&>li]:text-[var(--color-primary-50)] [&>li]:text-2xl [&>li]:cursor-pointer [&>li]:hover:underline [&>li]:hover:underline-offset-15">
-                        <li>Home</li>
-                        <li>Praias</li>
-                        <li>Previsão do Tempo</li>
-                        <li>Contato</li>
+                    <ul className="flex gap-10">
+                        <li className="text-[var(--color-primary-50)] text-2xl cursor-pointer relative group w-max">
+                            <span className="relative z-10">Home</span>
+                            <span className="absolute -bottom-1 left-0 w-0 transition-all duration-300 h-0.5 bg-[var(--color-primary-50)] group-hover:w-full"></span>
+                        </li>
+                        <li className="text-[var(--color-primary-50)] text-2xl cursor-pointer relative group w-max">
+                            <span className="relative z-10">Praias</span>
+                            <span className="absolute -bottom-1 left-0 w-0 transition-all duration-300 h-0.5 bg-[var(--color-primary-50)] group-hover:w-full"></span>
+                        </li>
+                        <li className="text-[var(--color-primary-50)] text-2xl cursor-pointer relative group w-max">
+                            <span className="relative z-10">Previsão do Tempo</span>
+                            <span className="absolute -bottom-1 left-0 w-0 transition-all duration-300 h-0.5 bg-[var(--color-primary-50)] group-hover:w-full"></span>
+                        </li>
+                        <li className="text-[var(--color-primary-50)] text-2xl cursor-pointer relative group w-max">
+                            <span className="relative z-10">Contato</span>
+                            <span className="absolute -bottom-1 left-0 w-0 transition-all duration-300 h-0.5 bg-[var(--color-primary-50)] group-hover:w-full"></span>
+                        </li>
                     </ul>
                 </nav>
                 <div className=' flex gap-5'>
                     <button onClick={() => navigate("/login")}
-                        className="cursor-pointer   px-12 py-3 rounded-xl border-[1px] border-white text-white font-medium group shadow-md transition-colors hover:bg-[var(--color-primary-800)]"
+                        className="cursor-pointer   px-12 py-3 rounded-xl border-[1px] border-white text-white font-medium group shadow-md transition-colors hover:bg-[var(--color-primary-800)]"
                         >
                         <div className="relative overflow-hidden">
-                            <p
-                            className="group-hover:-translate-y-7 duration-[1.125s] ease-[cubic-bezier(0.19,1,0.22,1)]"
+                            <p className="group-hover:-translate-y-7 duration-[1.125s] ease-[cubic-bezier(0.19,1,0.22,1)]"
                             >
                             Login
                             </p>
-                            <p
-                            className="absolute top-7 left-0 group-hover:top-0 duration-[1.125s] ease-[cubic-bezier(0.19,1,0.22,1)]"
+                            <p className="absolute top-7 left-0 group-hover:top-0 duration-[1.125s] ease-[cubic-bezier(0.19,1,0.22,1)]"
                             >
                             Login
                             </p>
@@ -36,7 +46,7 @@ const Header = () => {
                     </button>
 
                     <button onClick={() => navigate("/cadastro")}
-                        className="cursor-pointer   px-12 py-3 rounded-xl border-[1px] border-white text-white font-medium group shadow-md transition-colors hover:bg-[var(--color-primary-800)]"
+                        className="cursor-pointer   px-12 py-3 rounded-xl border-[1px] border-white text-white font-medium group shadow-md transition-colors hover:bg-[var(--color-primary-800)]"
                         >
                         <div className="relative overflow-hidden">
                             <p
@@ -51,8 +61,6 @@ const Header = () => {
                             </p>
                         </div>
                     </button>
-                    
-                    {/* <button onClick={() => navigate("/cadastro")} className='bg-[var(--color-primary-50)] py-3 px-5 rounded-4xl text-2xl cursor-pointer font-semibold shadow-md transition-colors  hover:bg-[var(--color-primary-800)] hover:text-[var(--color-primary-50)] duration-[500ms] '>Cadastro</button> */}
                 </div>
             </div>
         </header>
